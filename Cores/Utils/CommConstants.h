c7d929786a7088217615c0c42e69b8d625e9be44
朗月 <zhaoyu.qin@foxmail.com>
2023-08-03 11:10:57 +0800

    update coordinate

--- Cores/Utils/CommConstants.h
+++ Cores/Utils/CommConstants.h
@@ -12,31 +12,39 @@
 #include <cmath>
 
 
-// commonly used constants ------------------------------------------------------------
+// Commonly used constants ------------------------------------------------------------
 
 const int          _INT_MAX       = (std::numeric_limits<int>::max)();
+const int          _INT_MIN       = (std::numeric_limits<int>::min)();
 const unsigned int _UINT_MAX      = (std::numeric_limits<unsigned int>::max)();
+const unsigned int _UINT_MIN      = (std::numeric_limits<unsigned int>::min)();
 const double       _DBL_MAX       = (std::numeric_limits<double>::max)();
+const double       _DBL_MIN       = (std::numeric_limits<double>::min)();
 const double       _DBL_EPSILON   = (std::numeric_limits<double>::epsilon)();
 const float        _FLOAT_MAX     = (std::numeric_limits<float>::max)();
+const float        _FLOAT_MIN     = (std::numeric_limits<float>::min)();
 const float        _FLOAT_EPSILON = (std::numeric_limits<float>::epsilon)();
 
+#define IsNan(a) (isnan(a))
 #define IsEqual(a, b) (abs(a - b) <= _DBL_EPSILON)
 #define IsBigger(a, b) (a - b > _DBL_EPSILON)
 #define IsNoLessThan(a, b) (a - b >= _DBL_EPSILON)
 
 
-// single and double precision switches -----------------------------------------------
+// Single and double precision switches -----------------------------------------------
 
 #ifdef USE_SP
 typedef float real;
 const real    FP_EPSILON = _FLOAT_EPSILON;
 const real    FP_MAX     = _FLOAT_MAX;
+const real    FP_MIN     = _FLOAT_MIN;
 #else
 typedef double real;
 const real     FP_EPSILON = _DBL_EPSILON;
 const real     FP_MAX     = _DBL_MAX;
+const real     FP_MIN     = _DBL_MIN;
 #endif
 
 #define IsRealEqual(a, b) (abs(a - b) <= FP_EPSILON)
 #define IsRealBigger(a, b) (a - b > FP_EPSILON)
+#define IsRealNoLessThan(a, b) (a - b >= FP_EPSILON)
