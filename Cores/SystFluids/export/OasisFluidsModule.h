99800d0972ece0d3bfe77ee06ab2c40aff1ce434
朗月 <zhaoyu.qin@foxmail.com>
2023-08-09 16:02:49 +0800

    fix dll export bug

--- Cores/SystFluids/export/OasisFluidsModule.h
+++ Cores/SystFluids/export/OasisFluidsModule.h
@@ -1,22 +1,5 @@
 #pragma once
-
-// Define dll export macro.
-#ifdef __cplusplus
-#ifndef EXTERNC
-#define EXTERNC extern "C"
-#endif
-#else
-#define EXTERNC
-#endif
-
-#ifdef WINDOWS
-#ifndef EXPORT
-#define EXPORT EXTERNC __declspec(dllexport)
-#endif
-#else
-#define EXPORT EXTERNC
-#endif
-
+#include "Cores/Utils/CommMacros.h"
 
 /// @brief Get `RainfallModule` object which no external deletion is required.
 /// @param id Rainfall model id.
--- Cores/Utils/CommMacros.h
+++ Cores/Utils/CommMacros.h
@@ -30,6 +30,23 @@
 #include <stdexcept>
 #endif
 
+// Define dll export macro.
+#ifdef __cplusplus
+#ifndef EXTERNC
+#define EXTERNC extern "C"
+#endif
+#else
+#define EXTERNC
+#endif
+
+#ifdef WINDOWS
+#ifndef EXPORT
+#define EXPORT EXTERNC __declspec(dllexport)
+#endif
+#else
+#define EXPORT EXTERNC
+#endif
+
 // Define OO_ASSERT.
 #if defined(DEBUG) || defined(_DEBUG)
 #define OO_DEBUG_MODE
