3cac62e497f4aeb6b862dd72c80d5214a3a0b534
朗月 <zhaoyu.qin@foxmail.com>
2023-08-14 15:12:01 +0800

    init rl api

---
+++ OasisAgents/Hippo/modules/envs/ABCSpace.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+"""
+description
+
+__author__ = 'Qin zhaoyu'
+"""
+from abc import abstractmethod, ABCMeta
+
+
+class Space(ABCMeta):
+    """Basic class for all spaces."""
+
+    @abstractmethod
+    def sample(self, seed):
+        """
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def contains(self, x):
+        """
+        """
+        raise NotImplementedError()
