8d3af00b58dd7c4b67243aefcdb4f2228a476abd
朗月 <zhaoyu.qin@foxmail.com>
2023-08-15 10:00:07 +0800

    feat(Hippo): activate model

--- OasisAgents/Hippo/model.py
+++ OasisAgents/Hippo/model.py
@@ -1,9 +1,11 @@
 # -*- encoding: utf-8 -*-
 """
-description
+The `Model` component is responsible for integrating all the components 
+to form the entire pipeline.
 
 __author__ = 'Qin zhaoyu'
 """
+import pickle
 
 
 class Model:
@@ -13,3 +15,36 @@ class Model:
         self.net = net
         self.loss = loss
         self.optimizer = optimizer
+
+    def forward(self, inputs):
+        return self.net.forward(inputs)
+
+    def backward(self, predictions, targets):
+        loss = self.loss.loss(predictions, targets)
+        grad_from_loss = self.loss.grad(predictions, targets)
+        struct_grad = self.net.backward(grad_from_loss)
+        return loss, struct_grad
+
+    def apply_grads(self, grads):
+        params = self.net.params
+        self.optimizer.step(grads, params)
+
+    def save(self, path):
+        with open(path, "wb") as f:
+            pickle.dump(self.net.params, f)
+
+    def load(self, path):
+        with open(path, "rb") as f:
+            params = pickle.load(f)
+
+        self.net.params = params
+        for layer in self.net.layers:
+            layer.is_init = True
+
+    @property
+    def is_training(self):
+        return self.net.is_training
+
+    @is_training.setter
+    def is_training(self, is_training):
+        self.net.is_training = is_training
