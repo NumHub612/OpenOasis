57cee40d1fd7f93d397740eee47deea5f5d1eb69
朗月 <zhaoyu.qin@foxmail.com>
2023-08-15 14:14:23 +0800

    chore(Hippo): update

--- OasisAgents/Hippo/modules/__init__.py
+++ OasisAgents/Hippo/modules/__init__.py
@@ -2,5 +2,5 @@ from .layers.dense import Dense
 from .layers.relu import ReLU
 from .loss.mae import MAE
 from .loss.mse import MSE
-from .loss.SoftmaxCrossEntropy import SoftmaxCrossEntropy
+from .loss.sce import SoftmaxCrossEntropy
 from .optimizers.adam import Adam
