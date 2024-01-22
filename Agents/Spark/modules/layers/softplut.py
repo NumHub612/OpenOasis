46c58f7ddc41ff37898840bebc9c159e02d2f34d
朗月 <zhaoyu.qin@foxmail.com>
2024-01-22 17:23:43 +0800

    feat: transfer softplus activator

--- Agents/Spark/modules/layers/softplut.py
+++ Agents/Spark/modules/layers/softplut.py
@@ -5,13 +5,14 @@ Sigmoid activator layer.
 __author__ = 'borgwang'
 __url__ = https://github.com/borgwang/tinynn
 """
+import numpy as np
 from .ABCActivator import Activation
 from ...utils.math import sigmoid
 
 
-class Sigmoid(Activation):
+class Softplus(Activation):
     def func(self, x):
-        return sigmoid(x)
+        return np.log(1.0 + np.exp(-np.abs(x))) + np.maximum(x, 0.0)
 
     def derivative(self, x):
-        return self.func(x) * (1.0 - self.func(x))
+        return sigmoid(x)
