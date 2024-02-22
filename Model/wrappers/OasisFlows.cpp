43482c4070c76b4bfda57e901f0a6d26959a2798
朗月 <zhaoyu.qin@foxmail.com>
2024-02-22 22:15:19 +0800

    feat: remove hydrologics

--- Model/wrappers/OasisFlows.cpp
+++ Model/wrappers/OasisFlows.cpp
@@ -1,27 +1,19 @@
 #include "OasisFlows.h"
-// #include "Model/SystFluids/Hydrologics/RainfallModule.h"
-#include "Model/SystFluids/Hydrologics/RunoffModule.h"
-#include "Model/SystFluids/Hydrologics/RiverModule.h"
 #include "Model/SystHeats/HeatConductionModel/HeatConductionModel.h"
 
 
-using namespace OpenOasis::SystFluids::Hydrologics;
+// using namespace OpenOasis::SystFluids::Hydrologics;
 using namespace OpenOasis::SystHeats;
 using namespace std;
 
 static vector<shared_ptr<OpenOasis::ILinkableComponent>> componenents;
 
 
-void *GetRunoffComp(const char *id, const char *taskFile);
-void *GetRiverComp(const char *id, const char *taskFile);
 void *GetHeatConductionComp(const char *id, const char *taskFile);
 
 
 static unordered_map<string, function<void *(const char *, const char *)>>
-    componentFactory = {
-        {"RunoffComp", GetRunoffComp},
-        {"RiverComp", GetRiverComp},
-        {"HeatConductionComp", GetHeatConductionComp}};
+    componentFactory = {{"HeatConductionComp", GetHeatConductionComp}};
 
 
 void *GetComponent(const char *id, const char *type, const char *task)
@@ -32,22 +24,6 @@ void *GetComponent(const char *id, const char *type, const char *task)
     }
 
     return nullptr;
-}
-
-void *GetRunoffComp(const char *id, const char *taskFile)
-{
-    auto comp = make_shared<RunoffModule>(id, taskFile);
-    componenents.push_back(comp);
-
-    return comp.get();
-}
-
-void *GetRiverComp(const char *id, const char *taskFile)
-{
-    auto comp = make_shared<RiverModule>(id);
-    componenents.push_back(comp);
-
-    return comp.get();
 }
 
 void *GetHeatConductionComp(const char *id, const char *taskFile)
