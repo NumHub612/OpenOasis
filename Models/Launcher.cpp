44b2df4d1092edf7bfe6351197d9052f9686b657
朗月 <zhaoyu.qin@foxmail.com>
2024-04-01 16:56:26 +0800

    feat: import mpi

--- Models/Launcher.cpp
+++ Models/Launcher.cpp
@@ -18,6 +18,7 @@
 #include <iostream>
 #include <thread>
 #include <iomanip>
+#include <mpi.h>
 
 
 using namespace OpenOasis;
@@ -28,6 +29,28 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int  name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print off a hello world message
+    printf(
+        "Hello world from processor %s, rank %d out of %d processors\n",
+        processor_name,
+        world_rank,
+        world_size);
+
     // Setup command line arguments parser.
     args::ArgumentParser parser("OpenOasis component launcher");
     args::HelpFlag       help(parser, "", "OpenOasis help menu", {'h', "help"});
@@ -226,6 +249,9 @@ int main(int argc, const char *argv[])
     }
 
     spdlog::info("All components finished.");
+
+    // Finalize the MPI environment. No more MPI calls can be made after this
+    MPI_Finalize();
 
     return 0;
 }
