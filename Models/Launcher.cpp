26ef61148bf6cf3faab89fd5144651859f2e7af0
朗月 <zhaoyu.qin@foxmail.com>
2024-04-02 10:09:45 +0800

    fix: fix linux build bug

--- Models/Launcher.cpp
+++ Models/Launcher.cpp
@@ -29,28 +29,6 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
-    MPI_Init(NULL, NULL);
-
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int  name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
-
-    // Print off a hello world message
-    printf(
-        "Hello world from processor %s, rank %d out of %d processors\n",
-        processor_name,
-        world_rank,
-        world_size);
-
     // Setup command line arguments parser.
     args::ArgumentParser parser("OpenOasis component launcher");
     args::HelpFlag       help(parser, "", "OpenOasis help menu", {'h', "help"});
@@ -249,6 +227,29 @@ int main(int argc, const char *argv[])
     }
 
     spdlog::info("All components finished.");
+
+    // MPI test.
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int  name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print off a hello world message
+    printf(
+        "Hello world from processor %s, rank %d out of %d processors\n",
+        processor_name,
+        world_rank,
+        world_size);
 
     // Finalize the MPI environment. No more MPI calls can be made after this
     MPI_Finalize();
